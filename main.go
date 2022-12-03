f81d3ea6e5da3a1c3790ce802ef8294f79961077
ilies <iliesassaddev@gmail.com>
2022-12-03 01:49:06 +0100

    update

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"runtime"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// ConfigRuntime()
+	// var wg sync.WaitGroup
+	// wg.Add(1)
+	// startServers()
+	// wg.Wait()
+	StartGin("5000", "srv")
+}
+
+func startServers() {
+	log.Println("main starts")
+	go StartGin("3000", "srv p 80")
+	time.Sleep(1 * time.Second)
+	go StartGin("8080", "srv p 8080")
+	log.Println("server are running")
+}
+
+// ConfigRuntime sets the number of operating system threads.
+func ConfigRuntime() {
+	nuCPU := runtime.NumCPU()
+	runtime.GOMAXPROCS(nuCPU)
+	fmt.Printf("Running with %d CPUs\n", nuCPU)
+}
+
+// StartGin starts gin web server with setting router.
+func StartGin(port string, message string) {
+	// gin.SetMode(gin.ReleaseMode)
+
+	router := gin.New()
+	//router.Use(RateLimit, gin.Recovery())
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, message)
+	})
+
+	// port := os.Getenv("PORT")
+	// if port == "" {
+	// 	port = "8080"
+	// }
+	//log.Println("ENV PORT", os.Getenv("PORT"))
+	if err := router.Run(":" + port); err != nil {
+		log.Panicf("error: %s", err)
+	}
+}
